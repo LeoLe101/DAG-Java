c6117f2c63e364c1035ab05c3963decbca3f4eac
958016796 <958016796@LYN301-010.acs.edcc.ctc.edu>
2017-06-09 16:35:57 -0700

    create a new CourseManager class

---
+++ src/business/CourseManager.java
@@ -0,0 +1,22 @@
+package business;
+
+import dataaccess.Sorting;
+
+/**
+ * The class the that will 
+ * 
+ * @author Phuc Hong Le
+ * @author Son Minh Tran 
+ * @version 6/9/2017
+ */
+public class CourseManager {
+
+    private Sorting sort;
+    
+    public CourseManager() {
+    }
+    
+    //fields
+    
+    
+}
